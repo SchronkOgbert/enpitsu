6dc4e2517e57e87f35973d0cdf7d2a2d847200d8
weekendUM <sirbudamian@gmail.com>
2023-01-26 10:16:19 +0200

    Remove glew as conan dependency

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class EnpitsuConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     # default_options = {"shared": True, "fPIC": True}
     generators = "cmake", "cmake_find_package"
-    requires = 'glfw/3.3.8', 'glad/0.1.36', 'glew/2.2.0'
+    requires = 'glfw/3.3.8', 'glad/0.1.36'
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin") # From bin to bin
