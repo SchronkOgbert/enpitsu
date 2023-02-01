d57546a0602bfc1961c78d6f845f6f678453289f
weekendUM <sirbudamian@gmail.com>
2023-02-01 20:22:18 +0200

    initial shader program

--- include/ShaderProgram.h
+++ include/ShaderProgram.h
@@ -5,9 +5,20 @@
 #ifndef ENPITSU_SHADERPROGRAM_H
 #define ENPITSU_SHADERPROGRAM_H
 
+#include <glad/glad.h>
+#include <experimental/string>
 
 class ShaderProgram
 {
+    GLuint ID;
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    static char* readShaderFile(const char *filename);
+public:
+    ShaderProgram(const char* vertexFile, const char* fragmentFile);
+
+    virtual void Create();
+    virtual void Delete();
 
 };
 
--- src/ShaderProgram.cpp
+++ src/ShaderProgram.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "ShaderProgram.h"
+#include <fstream>
+#include <iostream>
+
+ShaderProgram::ShaderProgram(const char *vertexFile, const char *fragmentFile)
+{
+    const auto* vertexData = readShaderFile(vertexFile);
+    const auto* fragmentData = readShaderFile(fragmentFile);
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    ID = glCreateProgram();
+    std::cout << "compiling shader " << ID << "(" << vertexFile << ")\n";
+
+    glShaderSource(vertexShader, 1, &vertexData, nullptr);
+    glCompileShader(vertexShader);
+
+    glShaderSource(fragmentShader, 1, &fragmentData, nullptr);
+    glCompileShader(fragmentShader);
+
+    delete[] vertexData;
+    delete[] fragmentData;
+
+    glAttachShader(ID, vertexShader);
+    glAttachShader(ID, fragmentShader);
+
+    glLinkProgram(ID);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+}
+
+void ShaderProgram::Create()
+{
+    glUseProgram(ID);
+    std::cout << "compiled shader " << ID << '\n';
+}
+
+void ShaderProgram::Delete()
+{
+    glDeleteProgram(ID);
+}
+
+char* ShaderProgram::readShaderFile(const char *filename)
+{
+    std::ifstream fin(filename);
+    fin.seekg(0, std::ios::end);
+    const unsigned size = unsigned(fin.tellg()) + 2;
+    char* result = new char[size];
+    fin.seekg(0, std::ios::beg);
+    fin.read(result, size);
+    return result;
+}
