9ace327f20fb0786bb8360ae4960b8019d014987
weekendUM <sirbudamian@gmail.com>
2023-02-01 20:40:49 +0200

    initial vbo object

--- include/VBO.h
+++ include/VBO.h
@@ -5,10 +5,19 @@
 #ifndef LIBTEST_VBO_H
 #define LIBTEST_VBO_H
 
+#include "glad/glad.h"
 
 class VBO
 {
+    GLuint ID;
+public:
+    explicit VBO(GLfloat *vertices, const GLsizeiptr &size, const bool &isStatic = true);
 
+    void Bind() const;
+    void Unbind();
+    void Delete();
+
+    [[nodiscard]] GLuint getId() const;
 };
 
 
--- src/VBO.cpp
+++ src/VBO.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "VBO.h"
+
+GLuint VBO::getId() const
+{
+    return ID;
+}
+
+VBO::VBO(GLfloat *vertices, const GLsizeiptr &size, const bool &isStatic)
+{
+    glGenBuffers(1, &ID);
+    glBindBuffer(GL_ARRAY_BUFFER, ID);
+    glBufferData(GL_ARRAY_BUFFER, size, vertices, isStatic ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW); // NOLINT(clion-misra-cpp2008-5-0-4)
+}
+
+void VBO::Bind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, ID);
+}
+
+void VBO::Unbind()
+{
+    glBindBuffer(GL_ARRAY_BUFFER, static_cast<GLuint>(0));
+}
+
+void VBO::Delete()
+{
+    glDeleteBuffers(1, &ID);
+}
