561359f6706fb5e2fc7910a1712b992455102643
weekendUM <sirbudamian@gmail.com>
2023-02-02 20:42:59 +0200

    untested 2D object

--- include/Object2D.h
+++ include/Object2D.h
@@ -7,17 +7,32 @@
 
 #include "Object.h"
 #include "GeometryEssentials.h"
+#include "VAO.h"
+#include "EBO.h"
+#include "ShaderProgram.h"
+#include <memory>
 #include <vector>
 
 namespace enpitsu
 {
     class Object2D : public Object
     {
-        std::vector<Vector2> points;
+        std::vector<GLfloat > vertices;
+        std::vector<GLuint > indices;
+        std::unique_ptr<VAO> vao;
+        std::unique_ptr<VBO> vbo;
+        std::unique_ptr<EBO> ebo;
+        std::unique_ptr<ShaderProgram> shaderProgram;
     public:
         using Object::Object;
 
-        Object2D(Screen *screen, const std::vector<Vector2> &points, bool isStatic);
+        Object2D(Screen *screen, const std::vector<Vector2> &points,
+                 const bool &isStatic = true,
+                 const std::vector<unsigned int> &drawOrder = std::vector<unsigned int>(0U));
+
+        virtual void init() override;
+
+        virtual void onDestroy() override;
 
     protected:
         virtual void draw() override;
--- src/Object2D.cpp
+++ src/Object2D.cpp
@@ -7,10 +7,43 @@
 
 void enpitsu::Object2D::draw()
 {
-    screen;
+    shaderProgram->Create();
+    vao->Bind();
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, nullptr);
 }
 
-enpitsu::Object2D::Object2D(enpitsu::Screen *screen, const std::vector<Vector2> &points, bool isStatic) : Object(screen)
+enpitsu::Object2D::Object2D(Screen *screen, const std::vector<Vector2> &points, const bool &isStatic,
+                            const std::vector<unsigned int> &drawOrder) : Object(screen)
 {
+    this->vertices = std::vector<GLfloat>(points.size() * 2U);
+    if (drawOrder.empty())
+    {
+        this->indices = std::vector<GLuint>(this->vertices.size());
+        for(unsigned int i = 0U; i < this->indices.size(); i++)
+        {
+            this->indices[i] = i;
+        }
+    }
+    vao->Bind();
+    vbo = std::make_unique<VBO>(&vertices[0U], static_cast<GLsizeiptr>(vertices.size()), isStatic);
+    ebo = std::make_unique<EBO>(&indices[0U], static_cast<GLsizeiptr >(indices.size()), isStatic);
+    shaderProgram = std::make_unique<ShaderProgram>("default.vert", "default.frag");
+}
 
+void enpitsu::Object2D::init()
+{
+    Object::init();
+    vao->LinkVBO(*vbo, 0U);
+    vao->Unbind();
+    vbo->Unbind();
+    ebo->Unbind();
+}
+
+void enpitsu::Object2D::onDestroy()
+{
+    Object::onDestroy();
+    vao->Delete();
+    vbo->Delete();
+    ebo->Delete();
+    shaderProgram->Delete();
 }
