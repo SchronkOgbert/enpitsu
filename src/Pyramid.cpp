7bd57845b2577c27cddc954304fef62c77e05512
weekendUM <sirbudamian@gmail.com>
2023-04-29 21:10:31 +0300

    correct draw order for pyramid(so that normals can actually be drawn)

--- src/Pyramid.cpp
+++ src/Pyramid.cpp
@@ -16,9 +16,9 @@ namespace enpitsu
                         {1, 1, 1},
                         shader,
                         isStatic,
-                        new std::vector<unsigned>{0, 1, 2,
-                                                  0, 2, 3,
-                                                  0, 4, 1,
+                        new std::vector<unsigned>{2, 1, 0,
+                                                  3, 2, 0,
+                                                  0, 1, 4,
                                                   1, 2, 4,
                                                   2, 3, 4,
                                                   3, 0, 4}
